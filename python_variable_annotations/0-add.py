f6929b1b0fd160dca3485424a5c54554f48c56a2
khanimgurbanli <info@khanimgurbanli.com>
2024-08-06 00:02:12 +0400

    review code pycodestyle standarts

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,24 +1,13 @@
 #!/usr/bin/env python3
-""" Complex types - mixed list """
-from typing import List, Union
-"""
-Bu modül temel matematiksel işlemler için fonksiyonlar içerir.
-"""
+""" Basic annotations - add """
+
 
 def add(a: float, b: float) -> float:
-    """
-    İki ondalıklı sayıyı toplar ve sonucu döndürür.
-
-    Parametreler:
-    a (float): İlk sayı.
-    b (float): İkinci sayı.
-
-    Dönüş:
-    float: İki sayının toplamı.
+    """ Type-annotated function add that takes a float arguments.
+        Args:
+            a: float type.
+            b: float type.
+        Return:
+            sum as a float type.
     """
     return a + b
-
-
-
-
-
