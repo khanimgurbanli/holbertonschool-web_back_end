f96af4f739523be01d37d12dd0fcd0dddebee9f4
khanimgurbanli <info@khanimgurbanli.com>
2024-08-06 00:09:42 +0400

    review code pycodestyle standarts

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python3
+
 """
-This module contains functions for basic mathematical operations.
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1
+and each box may contain keys to the other boxes
+
+More info:
+- Prototype: def canUnlockAll(boxes)
+- boxes is a list of lists
+- A key with the same number as a box opens that box
+- You can assume all keys will be positive integers
+- The first box boxes[0] is unlocked
+- Return True if all boxes can be opened, else return False
 """
+
+
 def add(a: float, b: float) -> float:
+     """
+    Details:
+    Write a method that determines if all the boxes can be opened
+    Arguments:
+    boxes --> List of Lists, it contains the boxes with keys
+    Reurn boolean
+    Variables:
+    queue --> List, Store the number keys to open boxes
+    key --> integer, key of the myKeys
+    box --> integer, key inside of an specific box
     """
-    Add two floating point numbers and return the result.
-    """
-    return a + b
+     return a + b
 
 
 
