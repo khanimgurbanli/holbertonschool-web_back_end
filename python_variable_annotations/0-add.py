1c04f9592ec65592801f13d76f668987e5d8f77d
khanimgurbanli <info@khanimgurbanli.com>
2024-08-06 00:07:11 +0400

    review code pycodestyle standarts

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
-
 """
 This module contains functions for basic mathematical operations.
 """
-
-
 def add(a: float, b: float) -> float:
     """
     Add two floating point numbers and return the result.
