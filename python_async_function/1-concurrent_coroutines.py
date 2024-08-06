dcb0e1cb3c719976e9b8fd362b144072f637ef9d
khanimgurbanli <info@khanimgurbanli.com>
2024-08-06 16:58:33 +0400

    updated

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 
 """
-    Spawn `wait_random` n times with the specified max_delay and return the list of all delays in ascending order.
+    `wait_random` n times with the specified max_delay and return the list of all delays in ascending order
 
     Args:
         n (int): Number of times to spawn `wait_random`.
-        max_delay (int): The maximum delay in seconds for `wait_random`.
+        max_delay (int): The maximum delay in seconds for `wait_random`
 
     Returns:
-        List[float]: List of delays in ascending order.
+        List[float]: List of delays in ascending order
 """
 
 import asyncio
-import random
 from typing import List
 
 wait_random = __import__("0-basic_async_syntax").wait_random
