5b7fa35d413e813201375ddf698f16f0bd8e3a63
khanimgurbanli <info@khanimgurbanli.com>
2024-08-06 10:40:33 +0400

    7-to_kv.py

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from typing import Tuple, Union
+
+"""This module contains a function to sum up a list of integers and floating-point numbers."""
+
+from typing import Union, Tuple
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Creates a tuple where the first element is the string `k` and the second element
+    is the square of the integer or float `v`.
+
+    Parameters:
+    k (str): The key as a string.
+    v (Union[int, float]): The value to be squared; can be an integer or a float.
+
+    Returns:
+    Tuple[str, float]: A tuple where the first element is `k` and the second element
+                       is the square of `v` as a float.
+    """
+    return (k, float(v ** 2))
