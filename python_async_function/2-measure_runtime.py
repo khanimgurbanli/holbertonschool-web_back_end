74d1e45e277fd17bcf509b9b00f6f057a271637f
khanimgurbanli <info@khanimgurbanli.com>
2024-08-06 13:10:43 +0400

    2-measure_runtimes

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+
 import asyncio
 import time
 from typing import List
-wait_n = __import__('2-measure_runtime').wait_random
 
+wait_n = __import__('2-measure_runtime').wait_random
 
 def measure_time(n: int, max_delay: int) -> float:
     """ Measures the average execution time of wait_n function
