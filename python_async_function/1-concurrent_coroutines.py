06b6599b7b4baccb20fd6818d4baccd72f3ee0a0
khanimgurbanli <info@khanimgurbanli.com>
2024-08-06 12:29:26 +0400

    1-concurrent_coroutiness

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
-''' Description: Import wait_random from the previous python file that
-                 you’ve written and write an async routine called wait_n
-                 that takes in 2 int arguments: max_delay and n. You will
-                 spawn wait_random n times with the specified max_delay.
 
-                 wait_n should return the list of all the delays(float values)
-                 The list of the delays should be in ascending order without
-                 using sort() because of concurrency.
-    Arguments: n: int, max_delay: int = 10
-'''
+"""
+    Spawn `wait_random` n times with the specified max_delay and return the list of all delays in ascending order.
+
+    Args:
+        n (int): Number of times to spawn `wait_random`.
+        max_delay (int): The maximum delay in seconds for `wait_random`.
+
+    Returns:
+        List[float]: List of delays in ascending order.
+"""
 
 import asyncio
 import random
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
-
 
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """ Waits for ran delay until max_delay, returns list of actual delays """
@@ -28,4 +28,5 @@ async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     for spawn in spawn_ls:
         await spawn
 
+    # Use a min-heap approach to sort delays without using sort()
     return delay_ls
