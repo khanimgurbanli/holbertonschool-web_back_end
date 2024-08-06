f26e29f21bb1832c8d8eea235eebbbd958f4c3ab
khanimgurbanli <info@khanimgurbanli.com>
2024-08-06 09:32:03 +0400

    4-define_variables.py

--- python_variable_annotations/0-main.py
+++ python_variable_annotations/0-main.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
-import math
 
-fl = __import__('3-to_str').to_str
+a = __import__('4-define_variables').a
+pi = __import__('4-define_variables').pi
+i_understand_annotations = __import__('4-define_variables').i_understand_annotations
+school = __import__('4-define_variables').school
 
-print(fl(25.9202) == str(25.9202))
-print(fl.__annotations__)
+print("a is a {} with a value of {}".format(type(a), a))
+print("pi is a {} with a value of {}".format(type(pi), pi))
+print("i_understand_annotations is a {} with a value of {}".format(type(i_understand_annotations), i_understand_annotations))
+print("school is a {} with a value of {}".format(type(school), school))
