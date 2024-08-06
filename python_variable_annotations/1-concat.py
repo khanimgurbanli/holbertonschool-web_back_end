83dac0b0f195a947164f3e3c00cf8891649b5a63
khanimgurbanli <info@khanimgurbanli.com>
2024-08-06 11:35:12 +0400

    102-type_checking.pyy

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -5,4 +5,10 @@ This module contains a function for concatenating two strings.
 """
 
 def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings and return the result.
+    :param str1: first string
+    :param str2: second string
+    :return: concatenated string
+    """
     return str1 + str2
