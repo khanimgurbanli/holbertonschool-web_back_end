7ae4fbbfa4024dd1976d6fa0e528bd8402dd8e4b
khanimgurbanli <info@khanimgurbanli.com>
2024-08-08 21:50:19 +0400

    update

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -17,14 +17,16 @@ class BasicCache(BaseCaching):
 
 def put(self, key, item):
     """Add key-value pair to cache.
-    If 'key' or 'item' is None, do nothing"""
+    If 'key' or 'item' is None, do nothing
+    """
     if key is not None and item is not None:
         self.cache_data[key] = item
 
 
 def get(self, key):
     """Return value stored in 'key' of cache.
-    If key is None or does not exist in cache, return None"""
+    If key is None or does not exist in cache, return None
+    """
     if key is not None and key in self.cache_data:
         return self.cache_data[key]
     return None
