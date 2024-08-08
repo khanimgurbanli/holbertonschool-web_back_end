49b984c4c90f9364713e6a8d264d46303246bffc
khanimgurbanli <info@khanimgurbanli.com>
2024-08-08 14:43:27 +0400

    task 2

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Details: Write a coroutine called async_generator that takes no
+                 arguments.
+                 The coroutine will loop 10 times, each time asynchronously
+                 wait 1 second, then yield a random number between 0 and 10.
+                 Use the random module.
+"""
+
+import random
+import asyncio
+
+async_generator = __import__("0-async_generator").async_generator
+
+async def async_comprehension() -> list[float]:
+     return [value async for value in async_generator()]
+
