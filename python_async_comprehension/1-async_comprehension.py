09406e32a9f0365ca742fba6912959067fcc6d44
khanimgurbanli <info@khanimgurbanli.com>
2024-08-08 14:47:14 +0400

    task 2

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -13,4 +13,6 @@ async_generator = __import__("0-async_generator").async_generator
 
 
 async def async_comprehension() -> list[float]:
+    """Return result"""
     return [value async for value in async_generator()]
+
