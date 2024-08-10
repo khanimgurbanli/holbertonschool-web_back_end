517367313a586d8c3985367aac99e4368b276020
khanimgurbanli <info@khanimgurbanli.com>
2024-08-10 11:42:16 +0400

    100 edit

--- caching/100-lfu_cache.py
+++ caching/100-lfu_cache.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
-"""LFUCache that inherits from BaseCaching"""
+
+"""
+100. LFU caching
+"""
 
 BaseCaching = __import__("base_caching").BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """An LFU cache"""
+    """
+    Caching based on LRU (least frequently used)
+    """
 
     def __init__(self):
-        """Initialize class instance"""
+        """Initialize class instance."""
         super().__init__()
         self.keys = []
         self.uses = {}
 
     def put(self, key, item):
-        """Add key-value pair to cache data"""
+        """Add key/value pair to cache data.
+        If cache is at max capacity (specified by BaseCaching.MAX_ITEMS),
+        discard least frequently used entry to accommodate new entry."""
 
         if key is not None and item is not None:
             if len(self.keys) == BaseCaching.MAX_ITEMS and key not in self.keys:
-                discard = self.keys.pop(self.keys.index(self.findlfu()))
+                discard = self.keys.pop(self.keys.index(self.findLFU()))
                 del self.cache_data[discard]
                 del self.uses[discard]
                 print("DISCARD: {:s}".format(discard))
@@ -31,15 +38,18 @@ class LFUCache(BaseCaching):
                 self.uses[key] += 1
 
     def get(self, key):
-        """Return value stored in key of cache"""
+        """Return value stored in `key` key of cache.
+        If key is None or does not exist in cache, return None."""
         if key is not None and key in self.cache_data:
             self.keys.append(self.keys.pop(self.keys.index(key)))
             self.uses[key] += 1
             return self.cache_data[key]
         return None
 
-    def findlfu(self):
-        """Return key of least frequently used item"""
+    def findLFU(self):
+        """Return key of least frequently used item in cache.
+        If multiple items have the same amount of uses, return the least
+        recently used one."""
         items = list(self.uses.items())
         freqs = [item[1] for item in items]
         least = min(freqs)
