023134ce40032e6b40f2621bfd90d39cfae4cb2c
khanimgurbanli <info@khanimgurbanli.com>
2024-08-10 11:12:37 +0400

    100-lfu_cache.py

--- caching/100-lfu_cache.py
+++ caching/100-lfu_cache.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-""" LFU Caching Create a class LFUCache that inherits from BaseCaching
+""" LFU Caching: Create a class LFUCache that inherits from BaseCaching
                  and is a caching system"""
 
 BaseCaching = __import__("base_caching").BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """An LFU
-    Inherits all behaviors from BaseCaching except, upon any attempt to add
-    an entry to the cache when it is at max capacity (as specified by
-    BaseCaching.MAX_ITEMS), it discards the least frequently used entry to
-    accommodate for the new one.
-    Attributes:
-      __init__ - method that initializes class instance
-      put - method that adds a key/value pair to cache
-      get - method that retrieves a key/value pair from cache"""
+    """An LFU Cache.
+    Inherits all behaviors from BaseCaching except, upon any attempt
+    to add an entry to the cache when it is at max capacity (as specified
+    by BaseCaching.MAX_ITEMS), it discards the least frequently used entry
+    to accommodate for the new one. Attributes: __init__ - method that
+    initializes class instance put - method that adds a key/value pair to
+    cache get - method that retrieves a key/value pair from cache"""
 
     def __init__(self):
         """Initialize class instance."""
@@ -23,9 +21,9 @@ class LFUCache(BaseCaching):
         self.uses = {}
 
     def put(self, key, item):
-        """Add key/value pair to cache data
-        If cache is at max capacity (specified by BaseCaching.MAX_ITEMS),
-        discard least frequently used entry to accommodate new entry."""
+        """Add key/value pair to cache data. If cache is at max capacity
+        (specified by BaseCaching.MAX_ITEMS), discard least frequently
+        used entry to accommodate new entry."""
 
         if key is not None and item is not None:
             if len(self.keys) == BaseCaching.MAX_ITEMS and key not in self.keys:
@@ -42,8 +40,8 @@ class LFUCache(BaseCaching):
                 self.uses[key] += 1
 
     def get(self, key):
-        """Return value stored in `key` key of cache.
-        If key is None or does not exist in cache, return None."""
+        """Return value stored in `key` key of cache. If key is None or
+        does not exist in cache, return None."""
         if key is not None and key in self.cache_data:
             self.keys.append(self.keys.pop(self.keys.index(key)))
             self.uses[key] += 1
@@ -51,9 +49,8 @@ class LFUCache(BaseCaching):
         return None
 
     def findLFU(self):
-        """Return key of least frequently used item in cache.
-        If multiple items have the same amount of uses, return the least
-        recently used one"""
+        """Return key of least frequently used item in cache. If multiple
+        items have the same amount of uses, return the least recently used one"""
         items = list(self.uses.items())
         freqs = [item[1] for item in items]
         least = min(freqs)
