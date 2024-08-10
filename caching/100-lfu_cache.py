749f11aedd81bffc3b2167df82a5a03c548bd771
khanimgurbanli <info@khanimgurbanli.com>
2024-08-10 11:57:30 +0400

    100 editt

--- caching/100-lfu_cache.py
+++ caching/100-lfu_cache.py
@@ -1,63 +1,51 @@
 #!/usr/bin/python3
+""" LFU Caching """
+from collections import OrderedDict
 
-"""
-100. LFU caching
-"""
-
-BaseCaching = __import__("base_caching").BaseCaching
+from base_caching import BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """
-    Caching based on LRU (least frequently used)
-    """
+    """Class that inherits from BaseCaching and is a caching system"""
 
     def __init__(self):
-        """
-        Initialize class instance.
-        """
         super().__init__()
-        self.keys = []
-        self.uses = {}
+        self.lru_cache = OrderedDict()
+        self.lfu_cache = {}
 
     def put(self, key, item):
-        """
-        Add key/value pair to cache data.
-        """
-
-        if key is not None and item is not None:
-            if len(self.keys) == BaseCaching.MAX_ITEMS and key not in self.keys:
-                discard = self.keys.pop(self.keys.index(self.findLFU()))
-                del self.cache_data[discard]
-                del self.uses[discard]
-                print("DISCARD: {:s}".format(discard))
-            self.cache_data[key] = item
-            if key not in self.keys:
-                self.keys.append(key)
-                self.uses[key] = 0
+        """Assign to the dictionary, LFU algorithm"""
+        if key in self.lru_cache:
+            del self.lru_cache[key]
+        if len(self.lru_cache) > BaseCaching.MAX_ITEMS - 1:
+            min_value = min(self.lfu_cache.values())
+            lfu_keys = [k for k, v in self.lfu_cache.items() if v == min_value]
+            if len(lfu_keys) == 1:
+                print("DISCARD:", lfu_keys[0])
+                self.lru_cache.pop(lfu_keys[0])
+                del self.lfu_cache[lfu_keys[0]]
             else:
-                self.keys.append(self.keys.pop(self.keys.index(key)))
-                self.uses[key] += 1
+                for k, _ in list(self.lru_cache.items()):
+                    if k in lfu_keys:
+                        print("DISCARD:", k)
+                        self.lru_cache.pop(k)
+                        del self.lfu_cache[k]
+                        break
+        self.lru_cache[key] = item
+        self.lru_cache.move_to_end(key)
+        if key in self.lfu_cache:
+            self.lfu_cache[key] += 1
+        else:
+            self.lfu_cache[key] = 1
+        self.cache_data = dict(self.lru_cache)
 
     def get(self, key):
-        """
-        Return value stored in key of cache
-        """
-        if key is not None and key in self.cache_data:
-            self.keys.append(self.keys.pop(self.keys.index(key)))
-            self.uses[key] += 1
-            return self.cache_data[key]
-        return None
-
-    def findLFU(self):
-        """
-        Return key of least frequently used item in cache
-        """
-        items = list(self.uses.items())
-        freqs = [item[1] for item in items]
-        least = min(freqs)
-
-        lfus = [item[0] for item in items if item[1] == least]
-        for key in self.keys:
-            if key in lfus:
-                return key
+        """Return the value linked"""
+        if key in self.lru_cache:
+            value = self.lru_cache[key]
+            self.lru_cache.move_to_end(key)
+            if key in self.lfu_cache:
+                self.lfu_cache[key] += 1
+            else:
+                self.lfu_cache[key] = 1
+            return value
