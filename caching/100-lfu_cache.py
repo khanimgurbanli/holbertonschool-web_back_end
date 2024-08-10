ff465d68a985499a7fbc7bf26742221a659da6f1
khanimgurbanli <info@khanimgurbanli.com>
2024-08-10 12:03:19 +0400

    100 editt

--- caching/100-lfu_cache.py
+++ caching/100-lfu_cache.py
@@ -15,48 +15,37 @@ class LFUCache(BaseCaching):
 
     def put(self, key, item):
         """Assign to the dictionary, LFU algorithm"""
-        if key is None or item is None:
-            return
-
         if key in self.lru_cache:
             del self.lru_cache[key]
-
-        if len(self.lru_cache) >= BaseCaching.MAX_ITEMS:
-            # Find the least frequently used item
-            min_value = min(self.lfu_cache.values(), default=0)
+        if len(self.lru_cache) > BaseCaching.MAX_ITEMS - 1:
+            min_value = min(self.lfu_cache.values())
             lfu_keys = [k for k, v in self.lfu_cache.items() if v == min_value]
-
             if len(lfu_keys) == 1:
-                discard_key = lfu_keys[0]
+                print("DISCARD:", lfu_keys[0])
+                self.lru_cache.pop(lfu_keys[0])
+                del self.lfu_cache[lfu_keys[0]]
             else:
-                # If multiple items have the same frequency, discard the least recently used
-                discard_key = next(k for k in self.lru_cache if k in lfu_keys)
-
-            print("DISCARD:", discard_key)
-            self.lru_cache.pop(discard_key)
-            del self.lfu_cache[discard_key]
-
+                for k, _ in list(self.lru_cache.items()):
+                    if k in lfu_keys:
+                        print("DISCARD:", k)
+                        self.lru_cache.pop(k)
+                        del self.lfu_cache[k]
+                        break
         self.lru_cache[key] = item
         self.lru_cache.move_to_end(key)
-
-        # Update frequency
         if key in self.lfu_cache:
             self.lfu_cache[key] += 1
         else:
             self.lfu_cache[key] = 1
-
         self.cache_data = dict(self.lru_cache)
 
     def get(self, key):
         """Return the value linked"""
-        if key is None or key not in self.lru_cache:
-            return None
-
-        value = self.lru_cache[key]
-        self.lru_cache.move_to_end(key)
-        if key in self.lfu_cache:
-            self.lfu_cache[key] += 1
-        else:
-            self.lfu_cache[key] = 1
-
-        return value
+        if key in self.lru_cache:
+            value = self.lru_cache[key]
+            self.lru_cache.move_to_end(key)
+            if key in self.lfu_cache:
+                self.lfu_cache[key] += 1
+            else:
+                self.lfu_cache[key] = 1
+            return value
