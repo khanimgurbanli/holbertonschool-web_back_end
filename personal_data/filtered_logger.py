6558b8c6c6849ab6143ec9146cd35c61c1a882e7
Khanim Gurbanli <85050901+khanimgurbanli@users.noreply.github.com>
2024-08-15 10:11:06 +0000

    First task

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 """ Regex-ing, Log formatter, Create logger, Connect to secure database,
     Read and filter data """
-
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
-""" containing the fields from user_data.csv that are considered PII. """
-
 import re
 from typing import List
 
 
-def filter_datum(
-        fields: List[str], redaction: str, message: str, separator: str
-) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """
     Description: Writed a function called filter_datum that
                  returns the log message obfuscated"""
-    return re.sub(
-        f"({'|'.join(fields)})=.+?{separator}", r"\1=" + redaction + separator, message
-    )
+    return re.sub(f"({'|'.join(fields)})=.+?{separator}",
+                  r"\1=" + redaction + separator, message)
