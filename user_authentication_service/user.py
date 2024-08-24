7a135f31b0e24546cb99cf3cc50d2f1a14104978
khanimgurbanli <info@khanimgurbanli.com>
2024-08-24 15:44:13 +0400

    user_authentication_service

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -5,7 +5,6 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-
 class User(Base):
     """ SQLAlchemy model for a database table named users """
     __tablename__ = 'users'
