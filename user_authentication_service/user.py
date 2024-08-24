495ec96844222997e4013dd01ce4f570c95a8ba2
khanimgurbanli <info@khanimgurbanli.com>
2024-08-24 15:47:38 +0400

    user_authentication_service

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """ User model """
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
 
@@ -10,8 +9,7 @@ class User(Base):
     """ SQLAlchemy model for a database table named users """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
-
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
