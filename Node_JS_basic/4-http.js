732b0b19334c529098aa85e94feb983d93974e7b
khanimgurbanli <zeri.q.222@gmail.com>
2024-10-13 23:32:22 +0400

    add nodejs task

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,17 +1,12 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 1245;
-
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!\n');
 });
 
-app.listen(port, hostname, () => {
-  //   console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(1245, () => {
+    console.log('Listening on port 1245');
 });
 
 module.exports = app;
-
